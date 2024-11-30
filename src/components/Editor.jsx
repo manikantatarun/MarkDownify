abf1702abd2e6107f1af88542b56f65176e82e7a
manikantatarun <manikantatarun000@gmail.com>
2024-12-01 00:10:18 +0530

    Update Editor.jsx

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Editor({ content, onChange }) {
   return (
     <div className="editor-container">
-      <h2>Editor</h2>
+      <h2>Editor </h2>
       <textarea
         value={content}
         onChange={onChange}
